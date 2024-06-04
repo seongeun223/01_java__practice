6dbcc9c2517f2608cefb4634f819929a17c28801
seongeun223 <jklun080921@gmail.com>
2024-06-04 18:07:39 +0900

    friend

---
+++ chap16-lambda-practice/src/main/java/com/ohgiraffers/lambda/friend/Friend.java
@@ -0,0 +1,33 @@
+package com.ohgiraffers.lambda.friend;
+
+public class Friend {
+    private String name;
+    private int age;
+
+    public Friend() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Friend{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
---
+++ chap16-lambda-practice/src/main/java/com/ohgiraffers/lambda/friend/FriendApp.java
@@ -0,0 +1,15 @@
+package com.ohgiraffers.lambda.friend;
+
+public class FriendApp {
+    public static void main(String[] args) {
+        // 1. 다음 리스트를 순회하면서 40세 이상만 출력하세요. (list의 forEach메소드 사용할 것.)
+        List<Friend> list = List.of(
+                new Friend("홍길동", 30),
+                new Friend("신사임당", 48),
+                new Friend("전봉준", 35),
+                new Friend("세종대왕", 54),
+                new Friend("간달프", 105)
+        );
+    }
+    public 
+}
