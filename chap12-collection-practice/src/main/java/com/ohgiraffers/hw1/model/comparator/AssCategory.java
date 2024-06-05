87d1b41b0631f8648595bc980fd62bb9300ae10f
seongeun223 <jklun080921@gmail.com>
2024-06-05 16:50:47 +0900

    collection

---
+++ chap12-collection-practice/src/main/java/com/ohgiraffers/hw1/model/comparator/AssCategory.java
@@ -0,0 +1,16 @@
+package com.ohgiraffers.hw1.model.comparator;
+
+import java.util.Comparator;
+
+public class AssCategory implements Comparator<BookDTO> {
+    @Override
+    public int compare(BookDTO o1, BookDTO o2) {
+        int result = 0;
+        if(o1.getCategory() - o2.getCategory() > 0){
+            result = 1;
+        } else if(o1.getCategory() - o2.getCategory() < 0) {
+            result = -1;
+        }
+        return result;
+    }
+}
